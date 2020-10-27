bd465cf94350097a1241efeac288fc7b7cfc2330
mamitiana <mamitianaignace@gmail.com>
2020-10-27 20:54:50 +0300

    add timesteps to time to growth

--- SimulationDiscreteOneVariable.py
+++ SimulationDiscreteOneVariable.py
@@ -1,15 +1,19 @@
 a = 1.1
 x =1
+t = 0
+
 result = [x]
+timesteps = [t]
 from pylab import *
 
 def update(a, x):
-    return  a*x
+    return  a*x , t+0.1
 
 for i in range(30):
-    x = update(a, x)
+    x , t= update(a, x)
     result.append(x)
+    timesteps.append(t)
 print(result)
 
-plot(result)
+plot(timesteps,result)
 show()
